9337b5dbbb4a688bacff808b33926c63091b2430
shimojo <shimojo@no1s.biz>
2022-07-12 11:53:56 +0900

    first commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employees
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   first_name text NOT NULL,
+   last_name text NOT NULL,
+   email_address INT NOT NULL,
+   PRIMARY KEY(id)
+);
